9603887fd42f4499b4c728f2518523170dc59196
Арсений Бердюгин <berdyugin.arsen@yandex.ru>
2024-03-20 13:32:04 +0500

    Изменение нужных данных для входа

--- RegisterWindow.xaml.cs
+++ RegisterWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace ProfWorld
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
