db244979ade8d5b98821b50d4d7a9ba85b19e544
Андреев Дима <di_mit_id@mail.ru>
2024-04-09 02:48:08 +0500

    1-3 dizainer

--- Pages/Prof2/Prof2_1.xaml.cs
+++ Pages/Prof2/Prof2_1.xaml.cs
@@ -23,9 +23,127 @@ namespace ProfWorld.Pages.Prof2
         public Prof2_1()
         {
             InitializeComponent();
+
+            // Привязываем обработчик события изменения размеров окна
+            SizeChanged += MainWindow_SizeChanged;
+
+            // Инициализируем начальные размеры квадрата
+            UpdateSquareSize();
         }
 
-        private void next(object sender, RoutedEventArgs e)
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // При изменении размеров окна обновляем размеры квадрата
+            UpdateSquareSize();
+        }
+
+        private void UpdateSquareSize()
+        {
+            // Вычисляем минимальную сторону окна
+            double minSide = Math.Min(ActualWidth, ActualHeight);
+
+            // Устанавливаем размеры квадрата
+            square.Width = minSide;
+            square.Height = minSide;
+
+            // Перемещаем квадрат в центр окна
+            Canvas.SetLeft(square, (ActualWidth - minSide) / 2);
+            Canvas.SetTop(square, (ActualHeight - minSide) / 2);
+        }
+
+      
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            // Установить минимальное значение размера для StackPanel
+            double minSize = Math.Min(ActualWidth, ActualHeight);
+
+        }
+        private void Image_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Image draggedImage = sender as Image;
+            DataObject dragData = new DataObject(typeof(ImageSource), draggedImage.Source);
+            DragDrop.DoDragDrop(draggedImage, dragData, DragDropEffects.Copy);
+        }
+
+        private void GridDropTarget_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(typeof(ImageSource)))
+            {
+                // Получение данных перетаскивания (изображения)
+                Image droppedImage = new Image();
+                droppedImage.Source = e.Data.GetData(typeof(ImageSource)) as ImageSource;
+
+                // Добавление изображения в целевую ячейку
+                Border target = sender as Border;
+                target.Child = droppedImage;
+                target.Tag = droppedImage.Source;
+            }
+        }
+
+        private void CheckMatchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string text1 = "pack://application:,,,/ProfWorld;component/pages/prof2/img/";
+            // Переменная для отслеживания правильности расположения всех картинок
+            bool allMatchesCorrect = true;
+
+            // Проверяем соответствие каждой картинки и её ячейке
+            if (b1.Child == null || b1.Tag == null || b1.Tag.ToString() != text1 + "1.png")
+            {
+                allMatchesCorrect = false;
+            }
+            if (b2.Child == null || b2.Tag == null || b2.Tag.ToString() != text1 + "2.png")
+            {
+                allMatchesCorrect = false;
+            }
+            if (b3.Child == null || b3.Tag == null || b3.Tag.ToString() != text1 + "3.png")
+            {
+                allMatchesCorrect = false;
+            }
+            if (b4.Child == null || b4.Tag == null || b4.Tag.ToString() != text1 + "4.png")
+            {
+                allMatchesCorrect = false;
+            }
+            if (b5.Child == null || b5.Tag == null || b5.Tag.ToString() != text1 + "5.png")
+            {
+                allMatchesCorrect = false;
+            }
+            if (b6.Child == null || b6.Tag == null || b6.Tag.ToString() != text1 + "6.png")
+            {
+                allMatchesCorrect = false;
+            }
+            if (b7.Child == null || b7.Tag == null || b7.Tag.ToString() != text1 + "7.png")
+            {
+                allMatchesCorrect = false;
+            }
+            if (b8.Child == null || b8.Tag == null || b8.Tag.ToString() != text1 + "8.png")
+            {
+                allMatchesCorrect = false;
+            }
+            if (b9.Child == null || b9.Tag == null || b9.Tag.ToString() != text1 + "9.png")
+            {
+                allMatchesCorrect = false;
+            }
+
+            // Показываем сообщение о правильности или неправильности расположения всех картинок
+            if (allMatchesCorrect)
+            {
+                MessageBox.Show("Все картинки находятся в правильных ячейках.");
+                next1.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Некоторые картинки находятся в неправильных ячейках или ячейки пустые.");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Начните собирать крышу");
+            check.IsEnabled = false;
+        }
+
+
+private void next(object sender, RoutedEventArgs e)
         {
             {
                 // Получаем родительское окно страницы
--- Pages/Prof2/Prof2_3.xaml.cs
+++ Pages/Prof2/Prof2_3.xaml.cs
@@ -25,6 +25,62 @@ namespace ProfWorld.Pages.Prof2
             InitializeComponent();
         }
 
+        private void CheckAnswers_Click(object sender, RoutedEventArgs e)
+        {
+            bool[] answers = new bool[4]; // Массив для хранения правильности ответов
+
+            if (Answer1.IsChecked == true)
+                answers[0] = true;
+
+            if (Answer5.IsChecked == true)
+                answers[1] = true;
+
+            if (Answer8.IsChecked == true)
+                answers[2] = true;
+
+            if (Answer11.IsChecked == true)
+                answers[3] = true;
+
+            if (answers.Contains(false))
+            {
+                MessageBox.Show("В тесте есть ошибки. Проверьте свои ответы ещё раз.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("Все ответы верны! Поздравляем!", "Проверка завершена", MessageBoxButton.OK, MessageBoxImage.Information);
+                next1.IsEnabled = true;
+            }
+        }
+
+        private bool hintShown = false;
+
+        private void ShowHints_Click(object sender, RoutedEventArgs e)
+        {
+            if (!hintShown)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    HighlightCorrectAnswers(Answer1, Answer5, Answer8, Answer11);
+                });
+
+                hintShown = true;
+                ShowHints.IsEnabled = false;
+            }
+        }
+
+        private void HighlightCorrectAnswers(params RadioButton[] answers)
+        {
+            Random random = new Random();
+            foreach (var answer in answers)
+            {
+                if ((bool)answer.Tag)
+                {
+                    answer.Foreground = Brushes.Green;
+                }
+            }
+        }
+
+
         private void next(object sender, RoutedEventArgs e)
         {
             // Получаем родительское окно страницы
